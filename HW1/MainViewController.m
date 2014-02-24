9ba56a70cd0e8faae2409653d67c5604c586d4b9
zholdas <tzhe@yandex.ru>
2014-02-24 14:35:25 +0600

    first commit

---
+++ HW1/MainViewController.m
@@ -0,0 +1,34 @@
+//
+//  MainViewController.m
+//  HW1
+//
+//  Created by zholdasbek temirgaliyev on 24.02.14.
+//  Copyright (c) 2014 zholdasbek temirgaliyev. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+    int counter = 0;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)actionCount:(id)sender {
+    counter++;
+    self.lblCounter.text = [NSString stringWithFormat:@"%d", counter];
+}
+
+@end
